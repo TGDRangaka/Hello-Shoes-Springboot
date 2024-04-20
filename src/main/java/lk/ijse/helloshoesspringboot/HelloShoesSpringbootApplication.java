ce97f2e3bcfbdde2a45ad2eb9bc00ac56587dfe5
TGDRangaka <tgdilshanrangaka2002@gmail.com>
2024-04-20 09:45:10 +0530

    init project

---
+++ src/main/java/lk/ijse/helloshoesspringboot/HelloShoesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShoesSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloShoesSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/helloshoesspringboot/HelloShoesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.helloshoesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoesSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
